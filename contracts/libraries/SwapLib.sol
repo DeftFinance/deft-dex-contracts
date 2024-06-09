c981994c8ec34589f82a2fa6fa3ff22427f14185
root <root@DESKTOP-HSJGDN0.localdomain>
2024-06-09 16:44:27 +0330

    SwapLib Update

--- contracts/libraries/SwapLib.sol
+++ contracts/libraries/SwapLib.sol
@@ -3,22 +3,20 @@ pragma solidity 0.8.20;
 
 import {IDeftPair} from "../interfaces/IDeftPair.sol";
 
-
 library SwapLib {
 
     int32 public constant LOWER_THRESHOLD_LIMIT = -250;
-    uint32 public constant UPPER_THRESHOLD_LIMIT = 333;
-    uint32 public constant LOWER_STOP_LIMIT = -500;
-    uint32 public constant UPPER_STOP_LIMIT = 1000;
-    uint32 public constant BASIS_POINT = 1000;
+    int32 public constant UPPER_THRESHOLD_LIMIT = 334;
+    int32 public constant LOWER_STOP_LIMIT = -500;
+    int32 public constant UPPER_STOP_LIMIT = 1000;
 
     // Linear Regresion parameters
-    uint32 public constant REGRESSION_BASIS_POINT = 1_000_000;
-    int32 public constant A_LEFT = -184_478;
-    int32 public constant B_LEFT = -39_957;
+    int64 public constant REGRESSION_BASIS_POINT = 1_000_000;
+    int64 public constant A_LEFT = -184_478;
+    int64 public constant B_LEFT = -42_957;
 
-    int32 public constant A_RIGHT = 71_226;
-    int32 public constant B_RIGHT = -15_084;
+    int64 public constant A_RIGHT = 71_226;
+    int64 public constant B_RIGHT = -20_084;
 
     struct DeltaCalcParams{
         address token0;
@@ -31,40 +29,41 @@ library SwapLib {
 
     function applyDeltaAlgorithm(DeltaCalcParams calldata _deltaParams) public view returns(uint correctedFee) {
 
-        uint256 delta = _calculateDelta(_deltaParams);
+        int256 delta = _calculateDelta(_deltaParams);
         correctedFee = _calculateThreshold(delta, 3);
     }
 
     function _calculateDelta(
         DeltaCalcParams memory _deltaParams
     ) 
-        internal 
-        pure
-        returns(uint256 delta) 
+        public 
+        view
+        returns(int256 delta) 
     {
 
-        uint256 balance0 = token0.balanceOf(address(this));
-        uint256 balance1 = token1.balanceOf(address(this));
-        uint256 reserve0 = _deltaParams.reserve0;
-        uint256 reserve1 = _deltaParams.reserve1;
+        uint256 balance0 = IERC20(_deltaParams.token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(_deltaParams.token1).balanceOf(address(this));
+        
+        uint112 reserve0 = _deltaParams.reserve0;
+        uint112 reserve1 = _deltaParams.reserve1;
 
         // For the very first swap if the reserves are zero just return 0
         if(reserve0 == 0 || reserve1 == 0) {
-            return 0;
+            return (0);
         }
 
-        uint256 price_init = reserve0 / reserve1;
-        uint256 price_final = (_deltaParams.amount0Out > 0) ? 
-            (_deltaParams.amount0Out + reserve0) / (balance1) : 
-            (balance0) / (_deltaParams.amount1Out + reserve1);
+        uint256 price_init = 1000000 * _deltaParams.reserve0 / _deltaParams.reserve1;
+        uint256 price_final = (_deltaParams.amount0Out != 0) ? 
+            (1000000 * (_deltaParams.amount0Out + reserve0)) / (balance1) : 
+            (1000000 * balance0) / (_deltaParams.amount1Out + reserve1);
 
-        int256 delta = int(BASIS_POINT * price_final / price_init) - BASIS_POINT;
+        delta = (int256(1000 * price_final / price_init) - 1000);
 
     }
 
-    function _calculateModifiedFee(uint a, uint b, uint delta) internal pure returns(uint256 correctedFee) {
+    function _calculateModifiedFee(int64 a, int64 b, int delta) public pure returns(int256 correctedFee) {
 
-        return  3 + ((a * delta + b) / REGRESSION_BASIS_POINT);
+        return (((a * delta) + (1000 * b)) / REGRESSION_BASIS_POINT);
     }
 
     function _calculateStopCF() internal pure returns(uint correctedFee) {
@@ -73,10 +72,10 @@ library SwapLib {
     }
 
     function _calculateThreshold(
-        uint256 delta, 
-        uint inputFee
+        int256 delta, 
+        uint inputFee 
     )   
-        internal 
+        public 
         pure 
         returns(uint outputFee) 
     {
@@ -88,17 +87,17 @@ library SwapLib {
 
         else if(delta >= LOWER_STOP_LIMIT && delta < LOWER_THRESHOLD_LIMIT) {
             // a(delta) + b
-            outputFee = _calculateModifiedFee(A_LEFT, B_LEFT, delta);
+            outputFee = inputFee + uint256(_calculateModifiedFee(A_LEFT, B_LEFT, delta));
         }
 
         else if(delta > UPPER_THRESHOLD_LIMIT && delta <= UPPER_STOP_LIMIT) {
             // a(delta) + b
-            outputFee = _calculateModifiedFee(A_RIGHT, B_RIGHT, delta);
+            outputFee = inputFee + uint256(_calculateModifiedFee(A_RIGHT, B_RIGHT, delta));
         }
 
         else {
-            // set delta to 0.5 and apply higher gas fee
-            outputFee = _calculateStopCF()
+            // apply higher gas fee
+            outputFee = _calculateStopCF();
         }
     }
 }
